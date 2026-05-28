0bf8e2fa9329023519aa5241f1e6f1ccc752bd89
Kevin XU <160812274+godzillafan69420@users.noreply.github.com>
2026-05-28 12:39:16 +1200

    art and stuff

--- new-game-project/scripts/player.gd
+++ new-game-project/scripts/player.gd
@@ -3,15 +3,16 @@ extends Node2D
 
 
 
-var passives = [1,2,3,4,54,56]
-var img = preload("res://icon.svg")
+var passives = [1,2,3,4,54,56,"ninja","yes sir","cool","1000","t3"]
+var img = preload("res://Art/burgerpatty.png")
 const gap_size: float = 25
+const offset: float = 30
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 
-	for i in passives:
+	for i in passives.size():
 		var toppings = Sprite2D.new()
-		toppings.position =i* Vector2.UP * gap_size
+		toppings.position.y = i  * -gap_size - offset
 		toppings.texture = img
 		$toppings_group.add_child(toppings)
 
