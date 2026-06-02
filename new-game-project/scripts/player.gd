dfdba6e77d5fab10e172a249a2d488cc51a37397
Kevin XU <160812274+godzillafan69420@users.noreply.github.com>
2026-06-02 14:38:34 +1200

    attack animation

--- new-game-project/scripts/player.gd
+++ new-game-project/scripts/player.gd
@@ -9,7 +9,6 @@ const gap_size: float = 25
 const offset: float = 30
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-
 	for i in passives.size():
 		var toppings = Sprite2D.new()
 		toppings.position.y = i  * -gap_size - offset
