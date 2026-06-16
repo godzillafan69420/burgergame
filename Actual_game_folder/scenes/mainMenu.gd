75e295b14d21d1db9a6234a5062ea79a0c476030
Kevin XU <160812274+godzillafan69420@users.noreply.github.com>
2026-06-16 18:38:02 +1200

    what

--- Actual_game_folder/scenes/mainMenu.gd
+++ Actual_game_folder/scenes/mainMenu.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
+func _ready() -> void:                                                                                                                                                                                                                                                                                                                                                                             
 	AudioManager.play("MainMenu")
 	AudioManager.changeVolume()
 
