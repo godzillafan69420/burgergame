dd738d65a62eda12e48633f2ef7523a3331ddd5e
KallumWang <24634@burnside.school.nz>
2026-07-22 14:22:56 +1200

    wdefr

--- Actual_game_folder/ShopStuff/shop_scene.gd
+++ Actual_game_folder/ShopStuff/shop_scene.gd
@@ -24,9 +24,9 @@ var upgrade_pool = [
 # --- REGULAR ITEM POOL --- A
 var regular_item_pool = [
 	{"display_name": "tin foil","id": "iron_shield", "price": 4, "type": "defense", "icon": preload("res://Art/Tinfoil(card).png")},
-	{"display_name": "Frying Pan","id": "steel_sword", "price": 6, "type": "attack", "icon": preload("res://Art/CardTemplateTuff.png")},
+	{"display_name": "Frying Pan","id": "Frying_Pan", "price": 6, "type": "attack", "icon": preload("res://Art/CardTemplateTuff.png")},
 	{"display_name": "Heal","id": "health_potion", "price": 3, "type": "utility", "icon": preload("res://Art/Heal(card).png")},
-	{"display_name": "corn canon","id": "corn_canon", "price": 8, "type": "buff", "icon": preload("res://Art/CardTemplateTuff.png")},
+	{"display_name": "Corn Ball","id": "Corn_Ball", "price": 8, "type": "buff", "icon": preload("res://Art/Cornball(no_card).png")},
 	{"display_name": "Aura Farm","id": "we_see_the_fit", "price": 5, "type": "passive", "icon": preload("res://Art/WeSeeTheFit.png")},
 	{"display_name": "Hot Sauce","id": "hot_sauce", "price": 8, "type": "buff", "icon": preload("res://Art/CardTemplateTuff.png")},
 ]
