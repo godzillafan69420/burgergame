9885ce768038819c12e6c2b04e51e0a445f7036c
Kevin XU <160812274+godzillafan69420@users.noreply.github.com>
2026-08-10 21:40:18 +1200

    main menu art 5

--- Actual_game_folder/scenes/button.gd
+++ Actual_game_folder/scenes/button.gd
@@ -5,6 +5,7 @@ var background1 = preload("res://Art/gordoTitleScreen.png")
 var background2 = preload("res://Art/furry king mainmenu.png")
 var background3 = preload("res://Art/domainexpansion.png")
 var background4 = preload("res://Art/john vs socrates.png")
+var background5 = preload("res://Art/mainMenu caleb and jacon.png")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	AudioManager.play("MainMenu")
@@ -18,7 +19,7 @@ func _on_button_pressed() -> void:
 
 func _on_timer_timeout() -> void:
 	background_no += 1
-	if background_no > 3:
+	if background_no > 4:
 		background_no =0
 	if background_no == 0:
 		var tween = get_tree().create_tween()
@@ -49,6 +50,14 @@ func _on_timer_timeout() -> void:
 		tween.tween_property($CanvasLayer/TextureRect, "modulate", Color.BLACK, 1.0)
 		await get_tree().create_timer(1).timeout
 		$CanvasLayer/TextureRect.texture = background4
+		var reverse = get_tree().create_tween()
+		reverse.tween_property($CanvasLayer/TextureRect, "modulate", Color.WHITE, 1.0)
+		await get_tree().create_timer(1).timeout
+	if background_no == 4:
+		var tween = get_tree().create_tween()
+		tween.tween_property($CanvasLayer/TextureRect, "modulate", Color.BLACK, 1.0)
+		await get_tree().create_timer(1).timeout
+		$CanvasLayer/TextureRect.texture = background5
 		var reverse = get_tree().create_tween()
 		reverse.tween_property($CanvasLayer/TextureRect, "modulate", Color.WHITE, 1.0)
 		await get_tree().create_timer(1).timeout
