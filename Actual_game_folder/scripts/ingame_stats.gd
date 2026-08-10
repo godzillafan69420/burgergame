6e3b6c0c962c34deb6498970cc8c5d61b95cf111
Kevin XU <160812274+godzillafan69420@users.noreply.github.com>
2026-08-10 13:00:00 +1200

    balancing

--- Actual_game_folder/scripts/ingame_stats.gd
+++ Actual_game_folder/scripts/ingame_stats.gd
@@ -22,17 +22,16 @@ func _ready() -> void:
 	HP = PlayerStats.player_hitpoint
 	for i in PlayerStats.upgrades:
 		if i["id"] == "lettuce":
-			HP += 15
+			HP += 25
 		if i["id"] == "beef_patty":
 			upgraded_damage_multiplier += 0.15
 		if i["id"] == "cheese":
 			stamina_regeneration += 3
 		if i["id"] == "bacon":
-			hp_regeneration += 1
+			hp_regeneration += 2
 		if i["id"] == "pickle":
-			upgraded_damage_multiplier += 0.1
+			upgraded_damage_multiplier += 0.05
 			HP += 3
-			stamina_regeneration += 1
 	$HP.max_value = HP
 	$HP.value = HP
 	$Label.text = str(int($HP.value)) + "/" +str(int($HP.max_value))
--- Actual_game_folder/scripts/list_of_attacks.gd
+++ Actual_game_folder/scripts/list_of_attacks.gd
@@ -6,91 +6,105 @@ var punch = {
 	"name": "punch",
 	"damage": 10,
 	"status_effects": [],
-	"buff": []
+	"buff": [],
+	"animation": ""
 }
 
 var kick = {
 	"name": "kick",
 	"damage": 25,
 	"status_effects": [],
-	"buff": []
+	"buff": [],
+	"animation": ""
 }
 
 var poison = {
 	"name": "poison",
 	"damage": 0,
 	"status_effects":["poison"],
-	"buff": []
+	"buff": [],
+	"animation": ""
+	
 }
 
 var giga_punch = {
 	"name": "giga punch",
 	"damage": 30,
 	"status_effects": [],
-	"buff": []
+	"buff": [],
+	"animation": ""
 }
 
 var ragebait = {
 	"name": "ragebait",
 	"damage": 15,
 	"status_effects": ["weaken"],
-	"buff": []
+	"buff": [],
+	"animation": ""
 }
 
 var harmful_words = {
 	"name": "harmful words",
 	"damage": 15,
 	"status_effects": ["fatigue"],
-	"buff": []
+	"buff": [],
+	"animation": ""
 }
 
 var nuclear_pizza = {
 	"name": "nuclear pizza",
 	"damage": 5,
 	"status_effects": ["radiation"],
-	"buff": ["radiation"]
+	"buff": ["radiation"],
+	"animation": ""
 }
 
 var van = {
 	"name": "van",
 	"damage": 10,
 	"status_effects": ["weaken",],
-	"buff": []
+	"buff": [],
+	"animation": ""
 }
 
 var block = {
 	"name": "block",
 	"damage": 0,
 	"status_effects": [],
-	"buff": ["block"]
+	"buff": ["block"],
+	"animation": ""
 }
 
 var tickle = {
 	"name": "tickle",
 	"damage": 25,
 	"status_effects": ["weaken"],
-	"buff": []
+	"buff": [],
+	"animation": ""
 }
 
 var smelly_pasta = {
 	"name": "Smelly Pasta",
 	"damage": 35,
 	"status_effects": [],
-	"buff": []
+	"buff": [],
+	"animation": ""
 }
 
 var knife = {
 	"name": "knife",
 	"damage": 15,
 	"status_effects": ["bleed"],
-	"buff": []
+	"buff": [],
+	"animation": ""
 }
 
 var strong_right = {
 	"name": "Strong Right",
 	"damage": 50,
 	"status_effects": [],
-	"buff": []
+	"buff": [],
+	"animation": ""
 }
 
 var ice_cream_balls = {
@@ -104,19 +118,22 @@ var guess_what = {
 	"name": "guess what",
 	"damage": 0,
 	"status_effects": [],
-	"buff": ["block","heal"]
+	"buff": ["block","heal"],
+	"animation": ""
 }
 
 var chicken = {
 	"name": "chicken butt",
 	"damage": 30,
 	"status_effects": [],
-	"buff": ["heal"]
+	"buff": ["heal"],
+	"animation": ""
 }
 
 var banana_nuke = {
 	"name": "Super giga ultra mega giga again banananananana nuke",
 	"damage": 99999999,
 	"status_effects": [],
-	"buff": []
+	"buff": [],
+	"animation": ""
 }
